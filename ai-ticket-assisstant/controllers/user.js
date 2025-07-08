ce5515879225b325f82963f7158768712adcb002
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-07-08 20:11:08 +0530

    user controllers

--- ai-ticket-assisstant/controllers/user.js
+++ ai-ticket-assisstant/controllers/user.js
@@ -30,3 +30,84 @@ export const userSignup = async(req, res) => {
         })
     }
 }
+
+export const userLogin = async(req, res ) => {
+    const {email, password} = req.body;
+    try{
+        const foundData = await User.findOne({email});
+        if(!foundData) {
+            return res.status(401).json({
+                message: "User not found.",
+
+            })
+        }
+        const comaparison = await bcrypt.compare(password, foundData.password);
+        if(!comaparison) {
+            return res.status(400).json({
+                message: "Wrong credentials."
+            })
+        }
+
+        const token = jwt.sign({
+            _id: user._id, role: user.role
+        }, process.env.JWT_SECRET);
+
+        res.json({
+            user,
+            token
+        })
+
+    }catch(e) {
+        res.status(500).json({
+            message: "Login failed",
+            error: e
+        })
+    }
+}
+
+export const logout = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) return res.status(401).json({ error: "Unauthorzed" });
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) 
+            return res.status(401).json({ error: "Unauthorized" });
+        });
+        res.json({ message: "Logout successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Login failed", details: error.message });
+    }
+};
+
+
+export const updateUser = async (req, res) => {
+    const { skills = [], role, email } = req.body;
+    try {
+        if (req.user?.role !== "admin") {
+            return res.status(403).json({ eeor: "Forbidden" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) return res.status(401).json({ error: "User not found" });
+
+        await User.updateOne(
+            { email },
+            { skills: skills.length ? skills : user.skills, role }
+        );
+        return res.json({ message: "User updated successfully" });
+    } catch (error) {
+        res.status(500).json({ error: "Update failed", details: error.message });
+    }
+};
+
+export const getUsers = async (req, res) => {
+    try {
+        if (req.user.role !== "admin") {
+            return res.status(403).json({ error: "Forbidden" });
+        }
+
+        const users = await User.find().select("-password");
+        return res.json(users);
+    } catch (error) {
+        res.status(500).json({ error: "Update failed", details: error.message });
+    }
+};
