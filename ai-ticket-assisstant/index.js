a07e7bb9466f68a447f4cdff510e3f4d7dc988ec
Piyush_Bansal9 <piyushbansal130p@gmail.com>
2025-07-09 19:10:42 +0530

    Ticket routes and inngest middleware

--- ai-ticket-assisstant/index.js
+++ ai-ticket-assisstant/index.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import userRouter from "./routes/user.js"
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 dotenv.config();
 
@@ -12,9 +16,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
-app.use("/api/auth", userRouter)
+app.use("/api/auth", userRouter);
+app.use("/api/tickets", ticketRoutes);
 
-
+app.use(
+    "/api/inngest",
+    serve({
+        client: inngest,
+        functions: [onUserSignup, onTicketCreated],
+    })
+);
 
 mongoose
     .connect(process.env.MONGO_URL)
